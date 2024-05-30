df9a644bb0a0f92129e536deefcb892e7143a0f8
Saviour1001 <harshghodkar@gmail.com>
2024-05-30 23:09:36 +0530

    got some things working

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { ethers } from "ethers";
+import { BigNumberish, ethers } from "ethers";
 import { Squid } from "@0xsquid/sdk";
 
 const userAddress = "0xC03e0986879738BF7688b7Dc42C679BB498a530f";
 const integratorId = "squidframe-34ce7c50-ed06-4ac3-bc60-589277b02b76";
 
 const squid = new Squid({
-  baseUrl: "https://v2.api.squidrouter.com",
+  baseUrl: "https://api.squidrouter.com/",
   integratorId: integratorId,
 });
 
@@ -19,27 +19,29 @@ console.log(sendingAmount.toString());
 const { route } = await squid.getRoute({
   fromChain: "8453", // Base
   fromAmount: sendingAmount,
-  fromToken: "0xEEeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-  toChain: "10", // Optimism
-  toToken: "0xEEeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+  fromToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+  toChain: "314", // Filecoin
+  toToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
   fromAddress: userAddress,
   toAddress: userAddress,
-  slippageConfig: {
-    autoMode: 1,
-  },
+  slippage: 1.5,
 });
 
 console.log(route);
 
-const privateKey = "YOUR_PRIVATE_KEY";
+const privateKey = "YOUR KEY";
 const rpcUrl = "https://rpc.ankr.com/base"; // fromChain RPC URL
 
 const provider = new ethers.JsonRpcProvider(rpcUrl);
 const signer = new ethers.Wallet(privateKey, provider);
 
-const result = await squid.executeRoute({
-  route,
+// Execute the swap transaction
+const tx = (await squid.executeRoute({
   signer,
-});
+  route,
+})) as unknown as ethers.TransactionResponse;
+const txReceipt = await tx.wait();
 
-console.log(result);
+// Show the transaction receipt with Axelarscan link
+const axelarScanLink = "https://axelarscan.io/gmp/" + txReceipt?.hash;
+console.log(`Finished! Check Axelarscan for details: ${axelarScanLink}`);
